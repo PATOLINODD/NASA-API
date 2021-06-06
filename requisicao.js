7106fdfc1291bd4c32dead50537760b455803358
PATOLINODD <66355236+PATOLINODD@users.noreply.github.com>
2021-06-06 18:25:51 -0300

    subindo projeto API-DA-NASA

---
+++ requisicao.js
@@ -0,0 +1,21 @@
+	let botaoPesquisa 	= document.querySelector('#pesquisar');
+	const pesquisa 		= document.querySelector('input');
+
+	botaoPesquisa.onclick = () => {
+		document.querySelector('#explicacao').style.display = 'block';
+		console.log('botao pressionado');
+		enviarPedido();
+	}
+
+	async function enviarPedido(){
+		let resposta = await fetch(`https://api.nasa.gov/planetary/apod?api_key=xKNuJ9l2rSf2SOlvZposqh4J9TRhKsTVRSAgQkOy&date=${pesquisa.value}`);
+		let data = await resposta.json();
+		console.log(data);
+		mostrarPedido(data);
+	}
+	function mostrarPedido(data){
+		document.querySelector('#titulo').innerHTML = data.title;
+		document.querySelector('#data').innerHTML = "DATA: "+data.date;
+		document.querySelector('#img').innerHTML = `<img src='${data.hdurl}' width='50%'>`;
+		document.querySelector('#explicacao').innerHTML = data.explanation;
+	}
