bbb10071ab3ce35b8163579a5f5c783d335606cf
Patrick Soares <psomais.1997@gmail.com>
2021-11-12 11:58:43 -0300

    concertos erros e arrumando responsividade

--- requisicao.js
+++ requisicao.js
@@ -1,19 +1,24 @@
-	let botaoPesquisa 	= document.querySelector('#pesquisar');
-	const pesquisa 		= document.querySelector('input');
+let botaoPesquisa = document.querySelector("#pesquisar");
+const pesquisa = document.querySelector("input");
 
-	botaoPesquisa.onclick = () => {
-		document.querySelector('#explicacao').style.display = 'block';
-		enviarPedido();
-	}
+botaoPesquisa.onclick = () => {
+  document.querySelector("#explicacao").style.display = "block";
+  enviarPedido();
+};
 
-	async function enviarPedido(){
-		let resposta = await fetch(`https://api.nasa.gov/planetary/apod?api_key=xKNuJ9l2rSf2SOlvZposqh4J9TRhKsTVRSAgQkOy&date=${pesquisa.value}`);
-		let data = await resposta.json();
-		mostrarPedido(data);
-	}
-	function mostrarPedido(data){
-		document.querySelector('#titulo').innerHTML = data.title;
-		document.querySelector('#data').innerHTML = "DATA: "+data.date;
-		document.querySelector('#img').innerHTML = `<img src='${data.url}' width='50%'>`;
-		document.querySelector('#explicacao').innerHTML = data.explanation;
-	}
+async function enviarPedido() {
+  let resposta = await fetch(
+    `https://api.nasa.gov/planetary/apod?api_key=xKNuJ9l2rSf2SOlvZposqh4J9TRhKsTVRSAgQkOy&date=${pesquisa.value}`
+  );
+  let data = await resposta.json();
+  mostrarPedido(data);
+}
+function mostrarPedido(data) {
+  console.log(data);
+  document.querySelector("#titulo").innerHTML = data.title;
+  document.querySelector("#data").innerHTML = "DATA: " + data.date;
+  document.querySelector(
+    "#img"
+  ).innerHTML = `<img src='${data.hdurl}' width='50%'>`;
+  document.querySelector("#explicacao").innerHTML = data.explanation;
+}
